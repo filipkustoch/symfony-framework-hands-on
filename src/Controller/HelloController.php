786fe0fc80588b6a80dd9cf4e83d2a77dcd22bc9
filipkustoch <filip.kustoch1@gmail.com>
2024-01-07 18:17:28 +0100

    Twig Functions, Including Partial Templates

--- src/Controller/HelloController.php
+++ src/Controller/HelloController.php
@@ -9,36 +9,46 @@ use Symfony\Component\HttpKernel\Exception\NotFoundHttpException;
 
 class HelloController extends AbstractController
 {
-    private array $messages = ['Hello', 'Hi', 'Bye'];
+  private array $messages = [
+    ['message' => 'Hello', 'created' => '2023/06/12'],
+    ['message' => 'Hi', 'created' => '2023/04/12'],
+    ['message' => 'Bye!', 'created' => '2022/05/12']
+  ];
 
-    #[Route('/{limit<\d+>?2}', name: 'app_index')]
-    public function index(int $limit): Response
-    {
-        return $this->render(
-            'hello/index.html.twig',
-            [
-                'messages' => $this->messages,
-                'limit' => $limit
-            ]
-        );
-    }
+  #[Route('/{limit<\d+>?2}', name: 'app_index')]
+  public function index(int $limit): Response
+  {
+    return $this->render(
+      'hello/index.html.twig',
+      [
+        'messages' => $this->messages,
+        'limit' => $limit
+      ]
+    );
+  }
 
-    #[Route('/messages/{id<\d+>}', name: 'app_show_one_message')]
-    public function showOneMessage(int $id): Response
-    {
-        return $this->render(
-            'hello/show_one_message.html.twig',
-            [
-                'message' => $this->getMessageById($id),
-            ]
-        );
-    }
+  #[Route('/messages/{id<\d+>}', name: 'app_show_one_message')]
+  public function showOneMessage(int $id): Response
+  {
+    try {
+      $message = $this->getMessageById($id);
 
-    private function getMessageById(int $id): string
-    {
-        if (array_key_exists($id - 1, $this->messages)) {
-            return $this->messages[$id - 1];
-        }
-        throw new NotFoundHttpException('Message not found');
+      return $this->render(
+        'hello/show_one_message.html.twig',
+        [
+          'message' => $message,
+        ]
+      );
+    } catch (NotFoundHttpException $e) {
+      throw $this->createNotFoundException('Message not found', $e);
     }
+  }
+
+  private function getMessageById(int $id): array
+  {
+    if (array_key_exists($id - 1, $this->messages)) {
+      return $this->messages[$id - 1];
+    }
+    throw new NotFoundHttpException('Message not found');
+  }
 }
